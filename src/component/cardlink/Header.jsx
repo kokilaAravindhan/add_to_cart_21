6033f7d4a86965b8126fadf5ba3b1950743e9ba0
kokilaAravindhan <koks1.kvsd@gmail.com>
2023-07-10 22:50:15 +0530

    add_to_cart

---
+++ src/component/cardlink/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+//import style from './Header.css';
+function Header() {
+    //const name="cart"
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-4">
+                <a class="navbar-brand">Start BootCamp</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" >Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" >About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                         Shop </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" >All Products</a>
+                    <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" >Popular Items</a>
+                        <a class="dropdown-item" >New Arrivals</a>
+                    </div>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <button class="btn btn-outline-dark" type="submit">
+                        <i class="bi-cart-fill me-1"></i>
+                            Cart
+                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                    </button>
+                </form>
+ 
+      </div>
+      
+            </div>
+        </nav>
+    )
+}
+
+export default Header
